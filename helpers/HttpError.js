ddecf949f70d0d4f73efb7a89ac88d42f02eabd7
YuliiaVlasiuk <vlasyukyulia@gmail.com>
2023-05-29 23:52:54 +0300

    all homework

--- helpers/HttpError.js
+++ helpers/HttpError.js
@@ -1,12 +1,4 @@
-const errorMessageList = {
-  400: "Bad Request",
-  401: "Unauthorized",
-  403: "Forbidded",
-  404: "Not Found",
-  409: "Conflict",
-};
-
-const HttpError = (status, message = errorMessageList[status]) => {
+const HttpError = (status, message) => {
   const error = new Error(message);
   error.status = status;
   return error;
--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -9,18 +9,18 @@ const authenticate = async (req, res, next) => {
   const [bearer, token] = authorization.split(" ");
 
   if (bearer !== "Bearer") {
-    next(HttpError(401));
+    next(HttpError(401,"Unauthorized"));
   }
   try {
     const { id } = jwt.verify(token, SECRET_KEY);
     const user = await User.findById(id);
     if (!user || user.token !== token) {
-      next(HttpError(401));
+      next(HttpError(401,"Unauthorized"));
     }
     req.user = user;
     next();
   } catch {
-    next(HttpError(401));
+    next(HttpError(401,"Unauthorized"));
   }
 };
 
